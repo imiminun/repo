6c80e2b3c1f55fc89dfd0b8bbc0d810507ad00c1
mj <kkmj@inpix.com>
2023-06-08 18:05:20 +0900

    learn

---
+++ src/learn/script.js
@@ -0,0 +1,26 @@
+// const calculator = {
+//   plus: function(a, b) {
+//     console.log(a + b);
+//   },
+//   minus: function(a, b) {
+//     console.log(a - b);
+//   },
+//   times: function(a, b) {
+//     console.log(a * b);
+//   },
+//   divide: function(a, b) {
+//     console.log(a / b);
+//   },
+//   power: function(a, b) {
+//     console.log(a ** b);
+//   },
+// };
+
+const age = 96;
+function calculateKrAge(ageOfForeigner) {
+  return ageOfForeigner + 2;
+}
+
+const krAge =calculateKrAge(age);
+
+console.log(krAge);
